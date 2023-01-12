69016eac388dcb3ce63df6d7f82e9987730acf5c
Tetiana Chupika <mtv1301@gmail.com>
2023-01-12 18:25:23 +0200

    add GlobalExceptionHandler

--- src/main/java/springboot/example/gameofflags/dto/request/RegistrationRequest.java
+++ src/main/java/springboot/example/gameofflags/dto/request/RegistrationRequest.java
@@ -1,11 +1,17 @@
 package springboot.example.gameofflags.dto.request;
 
+import javax.validation.constraints.Size;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
 import springboot.example.gameofflags.validation.Email;
 import springboot.example.gameofflags.validation.Password;
 
 @Data
+@NoArgsConstructor
 public class RegistrationRequest {
+    @NonNull
+    @Size(min = 4)
     private String name;
     @Email()
     private String email;
